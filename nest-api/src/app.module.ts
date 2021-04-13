bdae4eddec7a3513b2ae0bf4e2bea137bfa09e6e
risaddex <risaddex@gmail.com>
2021-04-13 16:48:34 -0300

    added docker-compose

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+// ES7 decorators
 @Module({
   imports: [],
   controllers: [AppController],
