fbfd3ef137abb8ffc661cdb2ddc6159a42b0c770
risaddex <risaddex@gmail.com>
2021-04-13 12:34:13 -0300

    added .env

--- simulator/main.go
+++ simulator/main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	kafka "github.com/codeedu/imersaofsfc2-simulator/application/kafka"
 	route2 "github.com/risaddex/fullstackfullcycle2-simulator/application/route"
+	"log"
+	"github.com/joho/godotenv"
+
 )
 
-func main()  {
-	route := route2.Route{
-		ID: "1",
-		ClientID: "1",
+func init() {
+	// Loads local env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
 	}
-	route.LoadPositions()
-	stringjson, _ := route.ExportJsonPositions()
-	fmt.Println(stringjson[1])
+}
+
+func main()  {
+
+	producer := kafka.NewKafkaProducer()
+	kafka.Publish("ola", "readtest1", producer)
+	// route := route2.Route{
+	// 	ID: "1",
+	// 	ClientID: "1",
+	// }
+	// route.LoadPositions()
+	// stringjson, _ := route.ExportJsonPositions()
+	// fmt.Println(stringjson[1])
 }
