d12e55704628c5037aac676ac9732a70251f0e7f
risaddex <risaddex@gmail.com>
2021-04-13 16:59:43 -0300

    added routes controller

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoutesModule } from './routes/routes.module';
 // ES7 decorators
 @Module({
-  imports: [],
+  imports: [RoutesModule],
   controllers: [AppController],
   providers: [AppService],
 })
