0670eb26c05fca18c0eec64ce81764f7b530d738
risaddex <risaddex@gmail.com>
2021-04-16 15:15:09 -0300

    kafka-connect & elasticsearch

--- simulator/infra/kafka/consumer.go
+++ simulator/infra/kafka/consumer.go
@@ -15,7 +15,6 @@ type KafkaConsumer struct {
 }
 
 // NewKafkaConsumer creates a new KafkaConsumer struct with its message channel as dependency
-// ? Looks like a constructor
 func NewKafkaConsumer(msgChan chan *ckafka.Message) *KafkaConsumer {
 	return &KafkaConsumer{
 		MsgChan: msgChan,
@@ -27,25 +26,21 @@ func (k *KafkaConsumer) Consume() {
 	configMap := &ckafka.ConfigMap{
 		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
 		"group.id":          os.Getenv("KafkaConsumerGroupId"),
-		// "security.protocol": os.Getenv("security.protocol"),
-		// "sasl.mechanisms":   os.Getenv("sasl.mechanisms"),
-		// "sasl.username":     os.Getenv("sasl.username"),
-		// "sasl.password":     os.Getenv("sasl.password"),
+		"security.protocol": os.Getenv("security.protocol"),
+		"sasl.mechanisms":   os.Getenv("sasl.mechanisms"),
+		"sasl.username":     os.Getenv("sasl.username"),
+		"sasl.password":     os.Getenv("sasl.password"),
 	}
-	// c => consumer
 	c, err := ckafka.NewConsumer(configMap)
 	if err != nil {
 		log.Fatalf("error consuming kafka message:" + err.Error())
 	}
 	topics := []string{os.Getenv("KafkaReadTopic")}
-	// Kafka is listening our topics from here
 	c.SubscribeTopics(topics, nil)
 	fmt.Println("Kafka consumer has been started")
-	// GO's infinite loop is basically a empty for...
 	for {
 		msg, err := c.ReadMessage(-1)
 		if err == nil {
-			// Assigns this msg into  kafka's MsgChan channel
 			k.MsgChan <- msg
 		}
 	}
--- simulator/infra/kafka/producer.go
+++ simulator/infra/kafka/producer.go
@@ -3,6 +3,7 @@ package kafka
 import (
 	"log"
 	"os"
+
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
@@ -10,12 +11,11 @@ import (
 func NewKafkaProducer() *ckafka.Producer {
 	configMap := &ckafka.ConfigMap{
 		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
-		// "security.protocol": os.Getenv("security.protocol"),
-		// "sasl.mechanisms":   os.Getenv("sasl.mechanisms"),
-		// "sasl.username":     os.Getenv("sasl.username"),
-		// "sasl.password":     os.Getenv("sasl.password"),
+		"security.protocol": os.Getenv("security.protocol"),
+		"sasl.mechanisms":   os.Getenv("sasl.mechanisms"),
+		"sasl.username":     os.Getenv("sasl.username"),
+		"sasl.password":     os.Getenv("sasl.password"),
 	}
-	// producer
 	p, err := ckafka.NewProducer(configMap)
 	if err != nil {
 		log.Println(err.Error())
@@ -23,14 +23,12 @@ func NewKafkaProducer() *ckafka.Producer {
 	return p
 }
 
-// Publish is a simple function created to publish new message to kafka
+// Publish is simple function created to publish new message to kafka
 func Publish(msg string, topic string, producer *ckafka.Producer) error {
 	message := &ckafka.Message{
-		// Auto sets topic to kafka Partitions
 		TopicPartition: ckafka.TopicPartition{Topic: &topic, Partition: ckafka.PartitionAny},
 		Value:          []byte(msg),
 	}
-	// "nil" is a blank error
 	err := producer.Produce(message, nil)
 	if err != nil {
 		return err
